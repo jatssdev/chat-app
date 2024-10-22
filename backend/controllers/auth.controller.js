bb4c1d24945d30852df74ef07b8a9e0a586ef1c9
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-10-22 12:01:29 +0530

    .

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -48,8 +48,8 @@ export const signup = async (req, res) => {
 			res.status(400).json({ error: "Invalid user data" });
 		}
 	} catch (error) {
-		console.log("Error in signup controller", error.message);
-		res.status(500).json({ error: "Internal Server Error" });
+		console.log("Error in signup controller", error);
+		res.status(500).json({ error: "Internal Server Erroro" });
 	}
 };
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,12 +13,14 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+
 const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
